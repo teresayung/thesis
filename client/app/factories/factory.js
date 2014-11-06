b1f3b19ed14c595606a4283c939a3df761c8f5f0
Satoko <stogamori@gmail.com>
2014-11-05 20:10:05 -0800

    (feat) add factories of ServerRequests and ServerRoutes

--- client/app/factories/factory.js
+++ client/app/factories/factory.js
@@ -1,1 +1,35 @@
-factory.js
+angular.module('app.services', [])
+
+.factory('ServerRequests', function ($http) {
+  //make get request to the route
+  var get = function (route) {
+    return $http({
+      method: 'GET',
+      url: route
+    })
+    .then(function (resp) {
+      return resp.data;
+    });
+  };
+
+  var post = function (data, route) {
+    return $http({
+      method: 'POST',
+      url: route,
+      data: data
+    });
+  };
+
+  return {
+    get: get,
+    post: post
+  };
+})
+.factory('ServerRoutes', function(){
+  //returns the objects of the routes
+  return {
+    getPendings: 'someRoute',
+    sendVote: 'someRoute',
+    getResults: 'someRoute'
+  };
+});
