31a9e99a328ead02f7792dd9088d5d8a4a434b50
Teresa Yung <tereyung@gmail.com>
2014-11-06 20:21:19 -0800

    (refactor) add ionic and change architecture

---
+++ platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+/Users/teresayung/ThesisProject/project/platforms/android/ant-build/CordovaApp-debug-unaligned.apk : \
+/Users/teresayung/ThesisProject/project/platforms/android/ant-build/CordovaApp.ap_ \
+/Users/teresayung/ThesisProject/project/platforms/android/ant-build/classes.dex \
+/Users/teresayung/ThesisProject/project/platforms/android/CordovaLib/ant-build/classes.jar \
---
+++ platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,14 @@
+/Users/teresayung/ThesisProject/project/platforms/android/ant-build/classes.dex : \
+/Users/teresayung/ThesisProject/project/platforms/android/ant-build/classes/com/ionic/keyboard/IonicKeyboard$1.class \
+/Users/teresayung/ThesisProject/project/platforms/android/ant-build/classes/com/ionic/keyboard/IonicKeyboard$2.class \
+/Users/teresayung/ThesisProject/project/platforms/android/ant-build/classes/com/ionic/keyboard/IonicKeyboard$3.class \
+/Users/teresayung/ThesisProject/project/platforms/android/ant-build/classes/com/ionic/keyboard/IonicKeyboard.class \
+/Users/teresayung/ThesisProject/project/platforms/android/ant-build/classes/com/ionicframework/project580760/BuildConfig.class \
+/Users/teresayung/ThesisProject/project/platforms/android/ant-build/classes/com/ionicframework/project580760/CordovaApp.class \
+/Users/teresayung/ThesisProject/project/platforms/android/ant-build/classes/com/ionicframework/project580760/R$attr.class \
+/Users/teresayung/ThesisProject/project/platforms/android/ant-build/classes/com/ionicframework/project580760/R$drawable.class \
+/Users/teresayung/ThesisProject/project/platforms/android/ant-build/classes/com/ionicframework/project580760/R$string.class \
+/Users/teresayung/ThesisProject/project/platforms/android/ant-build/classes/com/ionicframework/project580760/R$xml.class \
+/Users/teresayung/ThesisProject/project/platforms/android/ant-build/classes/com/ionicframework/project580760/R.class \
+/Users/teresayung/ThesisProject/project/platforms/android/ant-build/classes/org/apache/cordova/device/Device.class \
+/Users/teresayung/ThesisProject/project/platforms/android/ant-build/dexedLibs/classes-8bd0f7a3406e480603980df738baae05.jar \
