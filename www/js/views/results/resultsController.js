1ec50db61f33db3c5f2f9e21a99ba9dbbe6c3d8d
Teresa Yung <tereyung@gmail.com>
2014-11-07 12:44:27 -0800

    (feat) add test file

---
+++ www/js/views/results/resultsController.js
@@ -0,0 +1,33 @@
+
+/* Results Controller:
+
+
+Expected Inputs:
+{loggedIn: true, userId: 1234}
+
+
+{resultContents: [{
+  topic: “string”, picture: undefined,
+  userId: 1234,
+  userName: ‘string’,
+  yes: #,
+  no: #
+  },
+  {},
+  {}
+  ]}
+
+*/
+
+//controllers should only contain business logic for a single view
+
+'use strict';
+
+
+angular.module('App.Results', [])
+
+.controller('ResultsController', ['$scope', function($scope){
+
+
+}])
+
