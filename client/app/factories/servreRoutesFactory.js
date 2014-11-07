9886f85ae7b2a28b7e5e1dcd782ba2771fdf98b9
Satoko <stogamori@gmail.com>
2014-11-06 16:50:34 -0800

    (refactor) separate factories

---
+++ client/app/factories/servreRoutesFactory.js
@@ -0,0 +1,11 @@
+angular.module('App.ServerRoutes', [])
+
+.factory('ServerRoutes', function(){
+  //return an object of the routes
+  return {
+    sendContent: 'someRoute',
+    getPending: 'someRoute',
+    sendVote: 'someRoute',
+    getResults: 'someRoute'
+  };
+});
