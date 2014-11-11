149b0e57c3f513a6f91d177a71d64eb944c669b4
Teresa Yung <tereyung@gmail.com>
2014-11-10 16:36:05 -0800

    (init) add karma.conf.js to integrate with grunt, tests are now connected

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,125 +1,215 @@
-module.exports = function(grunt) {
+module.exports = function(grunt){ //the "wrapper" function
 
+  /*========Project and task configurations======*/
   grunt.initConfig({
+    //Pkg imports the JSON metadata stored in package.json into the grunt config
     pkg: grunt.file.readJSON('package.json'),
-    concat: {
-      dist: {
-        src: [
-          'www/js/*.js'
-        ],
-        dest: 'production/production.js'
-      }
-    },
-
-    mochaTest: {
-      test: {
-        options: {
-          reporter: 'spec'
-        },
-        src: ['test/*.js']
-      }
-    },
-
-    // nodemon: {
-    //   dev: {
-    //     script: 'server.js'
-    //   }
-    // },
-
+    //Uglify minifies your code to be more space efficient
     uglify: {
-      build: {
-        src: 'production/production.js',
-        dest: 'production/production.min.js'
+      // options:{
+      //   mangle: false //set mangle: false to prevent changes to variable and function name
+      // },
+      my_target: {
+        files: {
+          'production/production.min.js' : ['production/production.js']
+        }
       }
     },
-
-    jshint: {
-      files: [
-        'www/js/*.js'
-      ],
+    //Concat puts all the files you need into one big file 
+    concat: {
       options: {
-        force: 'true',
-        jshintrc: '.jshintrc',
-        ignores: [
-          'www/lib/**/*.js',
-          'production/**/*.js'
-        ]
-      }
-    },
-
-    cssmin: {
-    },
-
-    watch: {
-      scripts: {
-        files: [
-          'www/js/*.js',
-          'www/lib/**/*.js',
-          'www/**/.html'
-        ],
-        tasks: [
-          'concat',
-          'uglify'
-        ]
+        separator: ';' //separates each file with a semicolon
       },
-      css: {
-        files: 'www/css/*.css',
-        tasks: ['cssmin']
+      dist: {
+        src: ['www/js/*.js'], //concats all .js files in js folder
+        dest: 'production/production.js' //puts all concatenated files into production.js in the production folder
       }
     },
-
-    shell: {
-      prodServer: {
-        command: 'git push azure master'
+    //Karma is a testing tool
+    karma: {
+      unit: {
+        options: {
+          configFile: 'karma.conf.js'
+        }
       }
+      
     },
-  });
+    //JSHINT is a tool used to check the files are coded correctly
+    jshint: {
+      files: ['www/js/*.js', 'www/**/*.html']
+    },
+    //Watch will emit a watch event when watched files or tasks are modified
+    watch: {
+      scripts:{
+        files: ['www/js/*.js', 'www/**/*.html']
+      },
+      tasks: ['concat', 'uglify'],
+      karma: {
+        files: [], //**** FILL IN
+        tasks: ['karma unit:run']
+      }
+    }, 
 
+  })
+
+  /*======Loading Grunt plugins and tasks======*/
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-karma');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-mocha-test');
-  grunt.loadNpmTasks('grunt-shell');
-  grunt.loadNpmTasks('grunt-nodemon');
 
-  grunt.registerTask('server-dev', function (target) {
-    // Running nodejs in a different process and displaying output on the main console
-    var nodemon = grunt.util.spawn({
-         cmd: 'grunt',
-         grunt: true,
-         args: 'nodemon'
-    });
-    nodemon.stdout.pipe(process.stdout);
-    nodemon.stderr.pipe(process.stderr);
-
-    grunt.task.run([ 'watch' ]);
-  });
-
-  ////////////////////////////////////////////////////
-  // Main grunt tasks
-  ////////////////////////////////////////////////////
-
-  grunt.registerTask('test', [
-    'mochaTest'
-  ]);
-
-  grunt.registerTask('build', [
-    'mochaTest', 'concat', 'uglify', 'jshint', 'server-dev'
-  ]);
-
-  grunt.registerTask('upload', function() {
-    if(grunt.option('prod')) {
-      grunt.task.run(['shell']);
-    } else {
-      grunt.task.run([ 'server-dev' ]);
-    }
-  });
-
-  grunt.registerTask('deploy', [
-    'mochaTest', 'concat', 'uglify', 'jshint', 'shell'
-  ]);
+  /*======Custom tasks======u*/
+  // grunt.registerTask('karma', ['karma']);
+}
 
 
-};
+/*Notes on the side when creating Grunt file*/
+/*
+
+option property may be specified to override built-in defaults,
+each target may have an options property which is specific to that target
+
+In uglify - Specify mangle: false to prevent changes to your variable and function names.
+*/
+
+
+///**********=======OLD GRUNT FILE === ********* do not delete below - using it as reference///
+// module.exports = function(grunt) {
+
+//   //Project Configurations
+//   grunt.initConfig({
+//     pkg: grunt.file.readJSON('package.json'),
+//     concat: {
+//       dist: {
+//         src: [
+//           'www/js/*.js'
+//         ],
+//         dest: 'production/production.js'
+//       }
+//     },
+
+//     mochaTest: {
+//       test: {
+//         options: {
+//           reporter: 'spec'
+//         },
+//         src: ['test/*.js']
+//       }
+//     },
+
+//     karma: {
+//       unit: {
+//         options: {
+//           files: ['test/**/*.js'],
+//           exclude:[],
+//           background: true //The background option will tell grunt to run karma in a child process so it doesn't block subsequent grunt tasks.
+
+//         }
+//       }
+//     },
+
+//     // nodemon: {
+//     //   dev: {
+//     //     script: 'server.js'
+//     //   }
+//     // },
+
+//     uglify: {
+//       build: {
+//         src: 'production/production.js',
+//         dest: 'production/production.min.js'
+//       }
+//     },
+
+//     jshint: {
+//       files: [
+//         'www/js/*.js'
+//       ],
+//       options: {
+//         force: 'true',
+//         jshintrc: '.jshintrc',
+//         ignores: [
+//           'www/lib/**/*.js',
+//           'production/**/*.js'
+//         ]
+//       }
+//     },
+
+//     cssmin: {
+//     },
+
+//     watch: {
+//       scripts: {
+//         files: [
+//           'www/js/*.js',
+//           'www/lib/**/*.js',
+//           'www/**/.html'
+//         ],
+//         tasks: [
+//           'concat',
+//           'uglify'
+//         ]
+//       },
+//       css: {
+//         files: 'www/css/*.css',
+//         tasks: ['cssmin']
+//       }
+//     },
+
+//     shell: {
+//       prodServer: {
+//         command: 'git push azure master'
+//       }
+//     },
+//   });
+
+//   grunt.loadNpmTasks('grunt-contrib-uglify');
+//   grunt.loadNpmTasks('grunt-contrib-jshint');
+//   grunt.loadNpmTasks('grunt-contrib-watch');
+//   grunt.loadNpmTasks('grunt-contrib-concat');
+//   grunt.loadNpmTasks('grunt-contrib-cssmin');
+//   grunt.loadNpmTasks('grunt-mocha-test');
+//   grunt.loadNpmTasks('grunt-shell');
+//   grunt.loadNpmTasks('grunt-nodemon');
+//   grunt.loadNpmTasks('grunt-karma'); // used to run karma/jasmine unit testing
+
+//   grunt.registerTask('server-dev', function (target) {
+//     // Running nodejs in a different process and displaying output on the main console
+//     var nodemon = grunt.util.spawn({
+//          cmd: 'grunt',
+//          grunt: true,
+//          args: 'nodemon'
+//     });
+//     nodemon.stdout.pipe(process.stdout);
+//     nodemon.stderr.pipe(process.stderr);
+
+//     grunt.task.run([ 'watch' ]);
+//   });
+
+//   ////////////////////////////////////////////////////
+//   // Main grunt tasks
+//   ////////////////////////////////////////////////////
+
+//   grunt.registerTask('test', [
+//     'mochaTest'
+//   ]);
+
+//   grunt.registerTask('build', [
+//     'mochaTest', 'concat', 'uglify', 'jshint', 'server-dev'
+//   ]);
+
+//   grunt.registerTask('upload', function() {
+//     if(grunt.option('prod')) {
+//       grunt.task.run(['shell']);
+//     } else {
+//       grunt.task.run([ 'server-dev' ]);
+//     }
+//   });
+
+//   grunt.registerTask('deploy', [
+//     'mochaTest', 'concat', 'uglify', 'jshint', 'shell'
+//   ]);
+
+
+// };
---
+++ karma.conf.js
@@ -0,0 +1,32 @@
+module.exports = function(config){
+  //Configurations
+  config.set({
+    //Base Path is the root path location that will be used to resolve all relative paths defined in files and exclude
+    basePath: '',
+
+    //Frameworks - list of frameworks you want to use for testing ie, jasmine, mocha, qunit
+    frameworks: ['jasmine'],
+
+    //Files - array of files or patterns to load in browser.  The files array determines which files are included in the browser and whilc files are watched and served by Karma
+    files: ['www/index.html', 'www/js/**/**.js', 'www/js/views/**/**.html', 'www/lib/**/**.js'],
+    //angular lib. ui-router, mocks,
+
+    //Exclude - array of files or patterns to exclude, karma.conf.hs typically gets excluded if it is in your file pathway
+    exclude: ['www/lib/ionic/**.js'],
+
+    //Port- web server port
+    port: 8080, //*** IS THIS NECESSARY WHEN WE USE IONIC SERVE??
+
+    //Log Level - level of logging - possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+    //Autowatch - enabling or disabling watching files and executing tests whenever one of these files changes
+    autoWatch: false,
+
+    //Browser - a list of browsers to launch and capture.  When Karma starts up, it will also start up each browser which is placed within this setting.  Once Karma is shut down, it will shutdown these browsers as well.  You can also capture any browser manually by opening the browser and visiting the URL where the Karma web server is listening (http://localhost:SOMEPORT , ie: 8080)
+    browsers: ['Chrome'],
+
+    //Single Run - if true, karma will start and capture all configured browsers, run tests and then exit with an exit code of 0 or 1 depending on whether all tests passed or any tests failed
+    singleRun: true
+  });
+};
