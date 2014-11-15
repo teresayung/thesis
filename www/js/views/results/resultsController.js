38a055ef949647ee28b0b6a7abeee7a6c605bb3a
Teresa Yung <tereyung@gmail.com>
2014-11-14 20:24:00 -0800

    (bug) update resultsController.js so it connects with the server

--- www/js/views/results/resultsController.js
+++ www/js/views/results/resultsController.js
@@ -4,29 +4,32 @@ angular.module('App.Results', [])
 
 //when minify to deploy, want to write controller with [] syntax to "protect" them
 .controller('ResultsController', function($scope, $window, $location, Auth, ServerRequests, ServerRoutes){ //Auth, ServerRequests, ServerRoutes are factories
-  
-  console.log("are you in controller?")
-  // if(!Auth.loggedIn()){ // checks if the user is loggedIn
-  //   console.log("user is logged in")
-  //   //Call a get request to object the list of results, use a promise to ensure all
-  //   //results are obtained before continuing
-  //   ServerRequests.get(ServerRoutes.getResults).then(function(response){
-  //     //Expect the ServerRequest to output an object with resultsContents property with an array of contents
-  //     console.log("inside promise")
-  //     $scope.results = response.resultsContents //this will give me the array of objects I want to display
-  //   })
-  // } else {
-  //   //user is not logged in
-  //   //route to login page
-       //$location.path('/login')
-  // }
+  console.log("are you in controller?");
 
+  var userId = $window.localStorage.getItem('userId');
+  if(!Auth.loggedIn()){ // checks if the user is loggedIn
+    //Call a post request with the userId to the server to get a list of results with that userId 
+    //results are obtained before continuing
+    ServerRequests.post({userId: userId}, ServerRoutes.getResults).then(function(response){
+      //Expect the ServerRequest to output an array of contents
+
+       //the for loop is to make it so that the recent items are displayed first
+      $scope.results = [];
+      for(var recent = response.length - 1; recent >= 0 ; recent--){
+        $scope.results.push(response[recent]);
+      }
+    })
+  } else {
+    //user is not logged in
+    //route to login page
+    $location.path('/login')
+  }
 
 ///========= Testing the html ===============///
 
-  var results = {resultsContents: [{topic: "prom dress", picture: 'http://s3.weddbook.com/t4/1/9/8/1981083/emerald-strapless-beaded-criss-cross-long-prom-dress.jpg', userId: 1, userName: 'treelala', yes: 4, no: 1}, {topic: "dinner", picture: 'http://www.ivstatic.com/files/et/imagecache/636/files/slides/0413.jpg', userId: 2, userName: 'satoko', yes: 5, no: 0}, {topic: "bubble tea?", picture: "http://upload.wikimedia.org/wikipedia/commons/a/a2/Bubble_Tea.png", userId: 3, userName: 'bace', yes: 600, no: 0}, {topic: "hot or not?", picture: 'http://o0tp7mzzn32msux2jkg8kga0.wpengine.netdna-cdn.com/images/old/6a017c3697a248970b01a3fccf4b2e970b-250wi.png', userId: 1, userName: 'rich', yes: 4, no: 1}]}
+  // var results = [{topic: "prom dress", picture: 'http://s3.weddbook.com/t4/1/9/8/1981083/emerald-strapless-beaded-criss-cross-long-prom-dress.jpg', userId: 1, userName: 'treelala', yes: 4, no: 1}, {topic: "dinner", picture: 'http://www.ivstatic.com/files/et/imagecache/636/files/slides/0413.jpg', userId: 2, userName: 'satoko', yes: 5, no: 0}, {topic: "bubble tea?", picture: "http://upload.wikimedia.org/wikipedia/commons/a/a2/Bubble_Tea.png", userId: 3, userName: 'bace', yes: 600, no: 0}, {topic: "hot or not?", picture: 'http://o0tp7mzzn32msux2jkg8kga0.wpengine.netdna-cdn.com/images/old/6a017c3697a248970b01a3fccf4b2e970b-250wi.png', userId: 1, userName: 'rich', yes: 4, no: 1}]
   
-  $scope.results = results.resultsContents;
+  // $scope.results = results;
   // console.table($scope.results, "scope results")
 
   $scope.routeToHome = function(){
