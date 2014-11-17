b1cb039105a2009fcc6e33d330e1c81b79bafd12
Satoko <stogamori@gmail.com>
2014-11-17 11:21:59 -0800

    (feat) add error message in login and signin views

--- www/js/views/login/loginController.js
+++ www/js/views/login/loginController.js
@@ -2,17 +2,23 @@ angular.module('App.Login', [])
 	.controller('LoginController', function ($scope, $window, $location, Auth) {
 		$scope.usernameInput;
 		$scope.passwordInput;
+		$scope.showLoginError = false;
 
 		$scope.logIn = function(username, password){
-			// Auth.login(username, password)
-			//   .then(function(response){
-			//   	//after logging in, route to home
-					console.log('login as ', username, password);
-          $location.path('/');
-			//   })
-			//   .catch(function(error){
-			//   	console.error(error);
-			//   });
+			Auth.login(username, password)
+			  .then(function(response){
+			  	if(response.error){
+			  		$scope.showLoginError = true;
+			  		$scope.loginError = response.error;
+			  	}else{
+				  	//after logging in, route to home
+						console.log('login as ', username, password);
+	          $location.path('/');
+			  	}
+			  })
+			  .catch(function(error){
+			  	console.error(error);
+			  });
 		}
 
 		$scope.signUp = function(){
--- www/js/views/signup/signupController.js
+++ www/js/views/signup/signupController.js
@@ -2,8 +2,10 @@ angular.module('App.Signup', [])
   .controller('SignupController', function ($scope, $window, $location, ServerRequests, ServerRoutes){
   	$scope.usernameInput;
   	$scope.passwordInput;
+    // $scope.phoneInput;
+    $scope.emailInput;
 
-  	$scope.signUp = function (username, password) {
+  	$scope.signUp = function (username, password, phone, email) {
 
   		// Auth.signup(username, password)
   		// .then(function (response) {
