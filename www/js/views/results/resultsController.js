4fb8cb8c3e718e24d75998533b49e7228bf5db13
Richard Guan <guan.rich@gmail.com>
2014-11-20 09:57:54 -0800

    (bug) fixed results communication with server

--- www/js/views/results/resultsController.js
+++ www/js/views/results/resultsController.js
@@ -2,6 +2,11 @@
 
 angular.module('App.Results', [])
 
+.config(function ($compileProvider) {
+  $compileProvider
+    .imgSrcSanitizationWhitelist(/^\s*(https?|blob|cdvfile|content|ftp|mailto|file|tel):|data:image\//);
+})
+
 //when minify to deploy, want to write controller with [] syntax to "protect" them
 .controller('ResultsController', function($scope, $window, $location, Auth, ServerRequests, ServerRoutes){ //Auth, ServerRequests, ServerRoutes are factories
   console.log("are you in controller?");
@@ -9,19 +14,25 @@ angular.module('App.Results', [])
   var userId = $window.localStorage.getItem('userId');
   //Call a post request with the userId to the server to get a list of results with that userId 
   //results are obtained before continuing
-  ServerRequests.post({userId: userId}, ServerRoutes.getResults).then(function(response){
+  ServerRequests.post({ userId: userId }, ServerRoutes.getResults)
+    .then(function(response){
     //Expect the ServerRequest to output an array of contents
-
+      console.log('calling serverRequest Post ');
      //the for loop is to make it so that the recent items are displayed first
-    $scope.results = [];
-    for(var recent = response.length - 1; recent >= 0 ; recent--){
-      $scope.results.push(response[recent]);
-    }
-  })
+      $scope.results = [];
+      for(var recent = response.length - 1; recent >= 0 ; recent--){
+        $scope.results.push(response[recent]);
+      }
+      console.log($scope.results);
+    })
+    .catch(function(error){
+      console.log(error);
+    })
+  
 
 ///========= Testing the html ===============///
 
-  // var results = [{topic: "prom dress", picture: 'http://s3.weddbook.com/t4/1/9/8/1981083/emerald-strapless-beaded-criss-cross-long-prom-dress.jpg', userId: 1, userName: 'treelala', yes: 4, no: 1}, {topic: "dinner", picture: 'http://www.ivstatic.com/files/et/imagecache/636/files/slides/0413.jpg', userId: 2, userName: 'satoko', yes: 5, no: 0}, {topic: "bubble tea?", picture: "http://upload.wikimedia.org/wikipedia/commons/a/a2/Bubble_Tea.png", userId: 3, userName: 'bace', yes: 600, no: 0}, {topic: "hot or not?", picture: 'http://o0tp7mzzn32msux2jkg8kga0.wpengine.netdna-cdn.com/images/old/6a017c3697a248970b01a3fccf4b2e970b-250wi.png', userId: 1, userName: 'rich', yes: 4, no: 1}]
+  // var results = [{topic: "prom dress", data: 'http://s3.weddbook.com/t4/1/9/8/1981083/emerald-strapless-beaded-criss-cross-long-prom-dress.jpg', userId: 1, userName: 'treelala', yes: 4, no: 1}, {topic: "dinner", picture: 'http://www.ivstatic.com/files/et/imagecache/636/files/slides/0413.jpg', userId: 2, userName: 'satoko', yes: 5, no: 0}, {topic: "bubble tea?", picture: "http://upload.wikimedia.org/wikipedia/commons/a/a2/Bubble_Tea.png", userId: 3, userName: 'bace', yes: 600, no: 0}, {topic: "hot or not?", picture: 'http://o0tp7mzzn32msux2jkg8kga0.wpengine.netdna-cdn.com/images/old/6a017c3697a248970b01a3fccf4b2e970b-250wi.png', userId: 1, userName: 'rich', yes: 4, no: 1}]
   
   // $scope.results = results;
   // console.table($scope.results, "scope results")
@@ -34,8 +45,3 @@ angular.module('App.Results', [])
     $location.path('/settings')
   }
 })
-
-
-
-
-
