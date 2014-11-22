d5b3b4cf570d9c467a00e9de4039ca8b642bb249
Teresa Yung <tereyung@gmail.com>
2014-11-22 09:36:25 -0800

    (bug) comment out unnecessary parts of resultsController

--- www/js/views/results/resultsController.js
+++ www/js/views/results/resultsController.js
@@ -30,13 +30,13 @@ angular.module('App.Results', [])
 
 ///========= Testing the html ===============///
 
-var results = [{topic: "prom dress", data: 'http://s3.weddbook.com/t4/1/9/8/1981083/emerald-strapless-beaded-criss-cross-long-prom-dress.jpg', userId: 3, userName: 'treelala', yes: 4, no: 4, contentId: 100}, {topic: "dinner", data: 'http://www.ivstatic.com/files/et/imagecache/636/files/slides/0413.jpg', userId: 2, userName: 'satoko', yes: 5, no: 4, contentId: 101}, {topic: "bubble tea?", data: "http://upload.wikimedia.org/wikipedia/commons/a/a2/Bubble_Tea.png", userId: 3, userName: 'bace', yes: 4, no: 1, contentId: 102}, {topic: "hot or not?", data: 'http://o0tp7mzzn32msux2jkg8kga0.wpengine.netdna-cdn.com/images/old/6a017c3697a248970b01a3fccf4b2e970b-250wi.png', userId: 1, userName: 'rich', yes: 1, no: 2, contentId: 103}]
-    //the for loop is to make it so that the recent items are displayed first
-  $scope.results = [];
+// var results = [{topic: "prom dress", data: 'http://s3.weddbook.com/t4/1/9/8/1981083/emerald-strapless-beaded-criss-cross-long-prom-dress.jpg', userId: 3, userName: 'treelala', yes: 4, no: 4, contentId: 100}, {topic: "dinner", data: 'http://www.ivstatic.com/files/et/imagecache/636/files/slides/0413.jpg', userId: 2, userName: 'satoko', yes: 5, no: 4, contentId: 101}, {topic: "bubble tea?", data: "http://upload.wikimedia.org/wikipedia/commons/a/a2/Bubble_Tea.png", userId: 3, userName: 'bace', yes: 4, no: 1, contentId: 102}, {topic: "hot or not?", data: 'http://o0tp7mzzn32msux2jkg8kga0.wpengine.netdna-cdn.com/images/old/6a017c3697a248970b01a3fccf4b2e970b-250wi.png', userId: 1, userName: 'rich', yes: 1, no: 2, contentId: 103}]
+//     //the for loop is to make it so that the recent items are displayed first
+//   $scope.results = [];
   
-  for(var recent = results.length - 1; recent >= 0 ; recent--){
-    $scope.results.push(results[recent]);
-  }
+//   for(var recent = results.length - 1; recent >= 0 ; recent--){
+//     $scope.results.push(results[recent]);
+//   }
 
   $scope.routeToHome = function(){
     $location.path('/')
