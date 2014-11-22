182a4ff1441736e6f3eaf8421060a6549fae48ae
bace135 <bryane@rocketship.com>
2014-11-21 16:54:45 -0800

    (feat) add friends and connecting friends functionality to server

--- www/js/views/addFriends/addFriendsController.js
+++ www/js/views/addFriends/addFriendsController.js
@@ -3,26 +3,31 @@ angular.module('App.AddFriends', [])
 .controller('AddFriendsController', function($scope, $window, $location, ServerRequests, ServerRoutes, AddFriendsFactory){
   //get userId from local storage
   var userId = $window.localStorage.getItem('userId');
+  //userId = 22222;
+
+  //route back to friends if back button is pushed
+  $scope.routeBack = function(){
+    $location.path('/friends');
+  }
 
   //set default amount of pending friend requests
-  $scope.requestsAmount = 0;
+  $scope.requestersAmount = 0;
 
   //determine the proper sentence ending for the amount of friend requests
-  $scope.sentenceEnd = AddFriendsFactory.getSentenceEnd($scope.requestsAmount);
+  // $scope.sentenceEnd = AddFriendsFactory.getSentenceEnd($scope.requestersAmount);
 
   //set default response to an attempt to add a friend
   $scope.requestResponse = '';
 
   //determines if a response message will show or not
   $scope.responseExists = function(){
-    var check = ($scope.requestResponse === '') ? false : true;
-    console.log(check);
-    return check;
+    return ($scope.requestResponse === '') ? false : true;
   }
 
   //make a friend request
   $scope.makeRequest = function(friendName){
-    ServerRequests.post({username: friendName}, ServerRoutes.requestFriend)
+    var dataToSend = {requesterId: userId, confirmerName: friendName}
+    ServerRequests.post(dataToSend, ServerRoutes.requestFriend)
       .then(function(response){
         $scope.requestResponse = response;
       })
@@ -33,25 +38,49 @@ angular.module('App.AddFriends', [])
   }
 
   //Storage array for all pending friend requests
-  $scope.allRequests = [];
+  $scope.allRequesters = [];
 
   //get all pending friend requests
-  //getRequest
-  //addFriend
-  //confirmFriend
   $scope.getPendingRequests = function(){
     ServerRequests.post({userId: userId}, ServerRoutes.checkFriendRequest)
       .then(function(response){
-        $scope.allRequests = response.requests;
+        $scope.allRequesters = response.friendRequesters;
+        $scope.requestersAmount = $scope.allRequesters.length;
+        $scope.sentenceEnd = AddFriendsFactory.getSentenceEnd($scope.requestersAmount);
       })
       .catch(function(error){
           console.error(error);
       });
+  }
+  $scope.getPendingRequests();
 
+  $scope.acceptFriend = function(requesterId, index){
+    var dataToSend = {userId: userId, requesterId: requesterId}
+    ServerRequests.post(dataToSend, ServerRoutes.confirmFriend)
+      .then(function(response){
+        console.log(response.text);
+        $scope.allRequesters.splice(index, 1);
+        $scope.requestersAmount = $scope.allRequesters.length;
+        console.log($scope.allRequesters);
+      })
+      .catch(function(error){
+          console.error(error);
+      });
   }
 
-  $scope.routeBack = function(){
-    $location.path('/friends');
+  $scope.declineFriend = function(requesterId, index){
+    var dataToSend = {userId: userId, requesterId: requesterId}
+    ServerRequests.post(dataToSend, ServerRoutes.declineFriend)
+      .then(function(response){
+        console.log(response.text);
+        $scope.allRequesters.splice(index, 1);
+        $scope.requestersAmount = $scope.allRequesters.length;
+        console.log($scope.allRequesters);
+      })
+      .catch(function(error){
+          console.error(error);
+      });
   }
+
 
 })
