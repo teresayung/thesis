94db56c1b9885129ce2dcee3fbaffebdae3af537
Teresa Yung <tereyung@gmail.com>
2014-11-25 12:30:57 -0800

    (feat) add cuter results picture

--- www/js/views/results/resultsController.js
+++ www/js/views/results/resultsController.js
@@ -83,38 +83,38 @@ angular.module('App.Results', [])
 
 ///========= Testing the html ===============///
 
-// var results = [{topic: "kitty", data: 'http://petlifepro.com/wp-content/uploads/2014/08/playful-kitten-6683.jpg', userId: 3, userName: 'treelala', yes: 4, no: 4, contentId: 100}, {topic: "akita", data: 'http://media-cache-ec0.pinimg.com/736x/b7/10/b4/b710b479efe2f9489eb08dadf53d8ff0.jpg', userId: 2, userName: 'satoko', yes: 5, no: 4, contentId: 101}, {topic: "alaskan malamute", data: "http://www.dogbreedslist.info/uploads/allimg/dog-pictures/Alaskan-Malamute-3.jpg", userId: 3, userName: 'bace', yes: 4, no: 1, contentId: 102}, {topic: "pomeranian", data: 'http://paranerds.com/wp-content/uploads/Boo-Cutest-Pomeranian-Puppy-in-the-World.jpg', userId: 1, userName: 'rich', yes: 1, no: 2, contentId: 103}]
-//     //the for loop is to make it so that the recent items are displayed first
-//   $scope.results = [];
+var results = [{topic: "kitty", data: 'http://petlifepro.com/wp-content/uploads/2014/08/playful-kitten-6683.jpg', userId: 3, userName: 'treelala', yes: 4, no: 4, contentId: 100}, {topic: "akita", data: 'http://media-cache-ec0.pinimg.com/736x/b7/10/b4/b710b479efe2f9489eb08dadf53d8ff0.jpg', userId: 2, userName: 'satoko', yes: 5, no: 4, contentId: 101}, {topic: "alaskan malamute", data: "http://www.dogbreedslist.info/uploads/allimg/dog-pictures/Alaskan-Malamute-3.jpg", userId: 3, userName: 'bace', yes: 4, no: 1, contentId: 102}, {topic: "should i get this?", data: 'http://www.afunpictures.com/wp-content/uploads/2014/08/puppies-wallpaper-for-iphone-7-266x400.jpg', userId: 1, userName: 'rich', yes: 3, no: 2, contentId: 103}]
+    //the for loop is to make it so that the recent items are displayed first
+  $scope.results = [];
   
-//   for(var recent = results.length - 1; recent >= 0 ; recent--){
-//     //set correct stamp
-//     if(results[recent].yes >= results[recent].no){
-//       results[recent].stamp = '../../img/yesstamp.png'
-//     } else{
-//       results[recent].stamp = '../../img/nostamp.png'
-//     }
+  for(var recent = results.length - 1; recent >= 0 ; recent--){
+    //set correct stamp
+    if(results[recent].yes >= results[recent].no){
+      results[recent].stamp = '../../img/yesstamp.png'
+    } else{
+      results[recent].stamp = '../../img/nostamp.png'
+    }
 
-//     var yesPercent =(results[recent].yes /(results[recent].yes + results[recent].no)) *100;
-//     var noPercent =(results[recent].no /(results[recent].yes + results[recent].no)) * 100;
+    var yesPercent =(results[recent].yes /(results[recent].yes + results[recent].no)) *100;
+    var noPercent =(results[recent].no /(results[recent].yes + results[recent].no)) * 100;
     
-//     var yesPercentRounded = 5 * Math.round(yesPercent/5);
-//     var noPercentRounded = 5 * Math.round(noPercent/5);
+    var yesPercentRounded = 5 * Math.round(yesPercent/5);
+    var noPercentRounded = 5 * Math.round(noPercent/5);
 
-//     console.log(yesPercentRounded, noPercentRounded, "percentages")
+    console.log(yesPercentRounded, noPercentRounded, "percentages")
 
-//     results[recent].yesbarclass = classes[yesPercentRounded];
-//     results[recent].nobarclass = classes[noPercentRounded];
+    results[recent].yesbarclass = classes[yesPercentRounded];
+    results[recent].nobarclass = classes[noPercentRounded];
     
 
-//     $scope.results.push(results[recent]);
+    $scope.results.push(results[recent]);
 
-//     //set correct bar for each result 
+    //set correct bar for each result 
 
 
     
-//     console.log(results, "results")
-  // }
+    console.log(results, "results")
+  }
 
 
 
