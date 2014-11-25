c1ed876556e95ba636f3fa277152367a30ad995a
Satoko <stogamori@gmail.com>
2014-11-25 11:43:57 -0800

    (feat) add server address

--- www/js/factories/serverRoutesFactory.js
+++ www/js/factories/serverRoutesFactory.js
@@ -4,16 +4,16 @@ angular.module('App.ServerRoutes', [])
   //return an object of the routes
   return {
   	//change the routes once our server is deployed.
-    sendContent: '/receivers/sendContent',
-    getPending: '/pending/getPending',
-    sendVote: '/pending/sendVote',
-    getResults: '/results/getResults',
-    getReceivers: '/receivers/getContacts',
-    postContent: '/receivers/sendContent',
-    checkUpdates: '/home/checkUpdates',
-    checkFriendRequest: '/friends/checkRequest',
-    requestFriend: '/friends/requestFriend',
-    confirmFriend: '/friends/confirmFriend',
-    declineFriend: '/friends/declineFriend'
+    sendContent: 'http://votallyserve.azurewebsites.net/receivers/sendContent',
+    getPending: 'http://votallyserve.azurewebsites.net/pending/getPending',
+    sendVote: 'http://votallyserve.azurewebsites.net/pending/sendVote',
+    getResults: 'http://votallyserve.azurewebsites.net/results/getResults',
+    getReceivers: 'http://votallyserve.azurewebsites.net/receivers/getContacts',
+    postContent: 'http://votallyserve.azurewebsites.net/receivers/sendContent',
+    checkUpdates: 'http://votallyserve.azurewebsites.net/home/checkUpdates',
+    checkFriendRequest: 'http://votallyserve.azurewebsites.net/friends/checkRequest',
+    requestFriend: 'http://votallyserve.azurewebsites.net/friends/requestFriend',
+    confirmFriend: 'http://votallyserve.azurewebsites.net/friends/confirmFriend',
+    declineFriend: 'http://votallyserve.azurewebsites.net/friends/declineFriend'
   };
 });
