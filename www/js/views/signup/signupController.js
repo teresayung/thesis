9f5403a853f4b39355f63401ff1101f9436d06b0
bace135 <bryane@rocketship.com>
2014-11-25 20:49:28 -0800

    (bug) friends pulls from db and signups do not if validation fails

--- www/js/views/signup/signupController.js
+++ www/js/views/signup/signupController.js
@@ -8,18 +8,24 @@ angular.module('App.Signup', [])
     $scope.signupError;
 
   	$scope.signUp = function (username, password, email) {
-  		Auth.signup(username, password, email)
-  		.then(function (error){
-        //if there is a error message from the server, show it to the user
-        if(error){
-          $scope.showSignupError = true;
-          $scope.signupError = error;
-        }else{
-          //if there is no error, route to home
-    			console.log('siginup and login as', username, password);
-    			$location.path('/');
-        }
-  		})
+      if (!username || !password || !email){
+        $scope.showSignupError = true;
+        $scope.signupError = 'There was an error with your sign up information';
+      } else {
+        console.log(username, password, email);
+    		Auth.signup(username, password, email)
+    		.then(function (error){
+          //if there is a error message from the server, show it to the user
+          if(error){
+            $scope.showSignupError = true;
+            $scope.signupError = error;
+          }else{
+            //if there is no error, route to home
+      			console.log('siginup and login as', username, password);
+      			$location.path('/');
+          }
+    		})
+      }
   	};
 
   });
