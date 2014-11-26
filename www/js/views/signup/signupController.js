f0fa590cb6eef10b14152787534cb4b3bb8b5bc2
Satoko <stogamori@gmail.com>
2014-11-25 18:02:40 -0800

    (bug) fix compare password

--- www/js/views/signup/signupController.js
+++ www/js/views/signup/signupController.js
@@ -2,11 +2,13 @@ angular.module('App.Signup', [])
   .controller('SignupController', function ($scope, $window, $location, Auth){
   	$scope.usernameInput;
   	$scope.passwordInput;
+    $scope.confirmInput;
     $scope.emailInput;
     $scope.showSignupError = false;
     $scope.signupError;
 
   	$scope.signUp = function (username, password, email) {
+      console.log(username, password, email)
   		Auth.signup(username, password, email)
   		.then(function (error){
         //if there is a error message from the server, show it to the user
